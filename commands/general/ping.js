1d032a857b53e04d9725163fe9fedaa5cb2da4b5
Re3ker <Re3ker@users.noreply.github.com>
2021-04-03 15:49:33 +0200

    deleted ping command

--- commands/general/ping.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'ping',
-  description: 'pong!',
-  category: 'general',
-  cooldown: 5,
-  async execute(message, args){
-    message.channel.send("Pong! `" + `${Date.now() - message.createdTimestamp}` + " ms`");
-  }
-}
